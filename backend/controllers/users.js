0471465a28d62b3c162f2c96eb18077778cdfc9e
Jack1ee7 <nikita.chukanov@gmail.com>
2022-12-17 00:20:18 +0500

    fix: make cookie secure

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -82,6 +82,7 @@ module.exports.login = (req, res, next) => {
       );
       res.cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
+        secure: true,
         httpOnly: true,
         sameSite: true,
       });
